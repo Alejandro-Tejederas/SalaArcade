b60b85e84a77065242da113a62e30e5037c53032
Alejandro Tejederas Carmona <atejcar669@g.educaand.es>
2025-12-02 10:29:51 +0100

    Día 2 de proyecto

--- src/main/java/model/Jugador.java
+++ src/main/java/model/Jugador.java
@@ -1,4 +1,19 @@
 package model;
 
 public class Jugador {
+    private static int contadorIds = 1;
+
+    private String nombre;
+    private final int id;
+    private int creditos;
+    private int partidasJugadas;
+
+    public Jugador(int id, int creditos, int partidasJugadas) {
+        this.nombre = nombre;
+        this.id = id;
+        this.creditos = creditos;
+        this.partidasJugadas = partidasJugadas;
+
+
+    }
 }
--- src/main/java/model/MaquinaArcade.java
+++ src/main/java/model/MaquinaArcade.java
@@ -2,10 +2,24 @@ package model;
 
 public class MaquinaArcade {
 
-    String nombre;
-    String genero;
-    int precioPartida;
-    int partidasJugadas;
-    String[] rankingJugadores;      //Array de tamaño 3
+    private String nombre;
+    private String genero;
+    private int precioPartida;
+    private int partidasJugadas;
+    private boolean activa;
+    private String[] rankingJugadores;      //Array de tamaño 3
+    private String[] rankingPuntuaciones;   //Array de tamaño 3
 
+    public MaquinaArcade(String Neonshooter, String disparos, int precioPartida) {
+        this.nombre = Neonshooter;
+        this.genero = disparos;
+        this.precioPartida = precioPartida;
+        this.partidasJugadas = 0;
+        this.activa = true;
+        this.rankingJugadores = new String[3];
+        this.rankingPuntuaciones = new String[3];
+
+
+
+    }
 }
