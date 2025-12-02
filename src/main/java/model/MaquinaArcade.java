210357466d54c941c6ccf2488221de57fa673643
Alejandro Tejederas Carmona <atejcar669@g.educaand.es>
2025-12-02 09:11:16 +0100

    Carga inicial del proyecto

--- src/main/java/model/MaquinaArcade.java
+++ src/main/java/model/MaquinaArcade.java
@@ -1,4 +1,11 @@
 package model;
 
 public class MaquinaArcade {
+
+    String nombre;
+    String genero;
+    int precioPartida;
+    int partidasJugadas;
+    String[] rankingJugadores;      //Array de tamaño 3
+
 }
