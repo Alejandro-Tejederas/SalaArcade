8de400f6851fa8cd50e87d7fa0bbf6e8a19d436a
Alejandro Tejederas Carmona <atejcar669@g.educaand.es>
2025-12-09 10:36:03 +0100

    Día 4/5 de proyecto

--- src/main/java/model/Jugador.java
+++ src/main/java/model/Jugador.java
@@ -8,16 +8,53 @@ public class Jugador {
     private int creditos;
     private int partidasJugadas;
 
-    public Jugador(String nombre, int creditosIniciales) {
-        this.nombre = nombre;
+    public Jugador() {
+        this.nombre = "Nombre jugador";
         this.id = contadorIds++;
         this.creditos = creditos;
         this.partidasJugadas = 0;
 
     }
 
-    public String getNombre() { return nombre; }
-    public int getId() { return id; }
-    public int getCreditos() { return creditos; }
-    public int getPartidasJugadas() { return partidasJugadas; }
+    public  Jugador(String nombre, int id, int creditos, int partidasJugadas) throws Exception {
+        this.nombre = nombre;
+        this.id = id;
+        this.creditos = creditos;
+        this.partidasJugadas = partidasJugadas;
+
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+
+    }
+    public int getCreditos() {
+        return creditos;
+    }
+
+    public void setCreditos(int creditos) {
+        this.creditos = creditos;
+    }
+
+    public int getPartidasJugadas() {
+        return partidasJugadas;
+    }
+
+    public void setPartidasJugadas(int partidasJugadas) {
+        this.partidasJugadas = partidasJugadas;
+    }
+
+    public void
 }
+
--- src/main/java/model/MaquinaArcade.java
+++ src/main/java/model/MaquinaArcade.java
@@ -4,21 +4,26 @@ public class MaquinaArcade {
 
     private String nombre;
     private String genero;
-    private int precioPartida;
-    private int partidasJugadas;
+    private int precioPorPartida;
     private boolean activa;
+    private int contadorPartidas;
+
+    private int[] rankingPuntuaciones;      //Array de tamaño 3
     private String[] rankingJugadores;      //Array de tamaño 3
-    private String[] rankingPuntuaciones;   //Array de tamaño 3
+
+    private Random random = new Random();
 
     public MaquinaArcade(String nombre, String genero, int precioPartida) {
         this.nombre = nombre;
         this.genero = genero;
-        this.precioPartida = precioPartida;
-        this.partidasJugadas = 0;
+        this.precioPorPartida = precioPorPartida;
         this.activa = true;
+        this.contadorPartidas = 0;
+
 
         this.rankingJugadores = new String[3];
-        this.rankingPuntuaciones = new String[3];
+        this.rankingPuntuaciones = new int[3];
+
 
         public void activar() {
             this.activa = true;
@@ -32,24 +37,51 @@ public class MaquinaArcade {
             return activa;
         }
 
-        public int jugarPartida(String nombre){
+
+        public int jugarPartida(String nombreJugador){
 
             // Generar puntuación aleatoria 0-9999
             int puntuacion = random.nextInt(10000);
 
             //Incrementar contador de partidas
-            partidasJugadas++;
+            contadorPartidas++;
 
             //Si el contador es múltiplo de 100
-            if(partidasJugadas % 100 == 0){
+            if(contadorPartidas % 100 == 0){
                 desactivar();
 
             }
 
+            actualizarRanking(nombreJugador, puntuacion);
 
+            return puntuacion
 
         }
 
 
+        //Actualizar el raking top 3
+
+        private void actualizarRanking(String nombreJugador, int puntuacion){
+
+            for(int i = 0; i < 3; i++){
+                if(puntuacion == rankingPuntuaciones[i]){
+
+            for(int j=2; j>i; j--)
+                rankingPuntuaciones[j] = rankingPuntuaciones[j-1];
+                rankingJugadores[j] = rankingJugadores;
+            }
+
+
+                rankingPuntuaciones[i] = puntuacion;
+                rankingJugadores[i] = nombreJugador;
+                break;
+        }
     }
+}
+
+            public void mostrarInfo() {
+    System.out.println("----- Máquina Arcade -----");
+        System.out.println("Nombre: " + nombre);
+        System.out.println("Genero: " + genero);
+            }
 }
